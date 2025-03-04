3d6ad204e5cf585e555d3944b2c9e3d714dba3e1
Joshua Belden <jcbelden@procaresoftware.com>
2025-03-04 11:59:34 -0700

    Initial setup of client and server projects

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
+
+const config = {
+    type: Phaser.AUTO,
+    width: "100%",
+    height: "100%",
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        Game,
+        GameOver
+    ]
+};
+
+export default new Phaser.Game(config);
---
+++ client/src/scenes/Game.js
@@ -0,0 +1,26 @@
+import { Scene } from "phaser"
+
+export class Game extends Scene {
+    constructor() {
+        super("Game")
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x00ff00)
+
+        this.add
+            .text(512, 384, "Crisis", {
+                fontFamily: "Arial Black",
+                fontSize: 38,
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 8,
+                align: "center",
+            })
+            .setOrigin(0.5)
+
+        this.input.once("pointerdown", () => {
+            this.scene.start("GameOver")
+        })
+    }
+}
